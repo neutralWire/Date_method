43bdd2ac752d5e462afce78dfe664582ae47fec3
Shantanu Rajput <90391285+neutralWire@users.noreply.github.com>
2022-08-13 13:15:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const d = new Date();
+let year=d.getFullYear();
+let month=d.getMonth();
+let date=d.getDate();
+let hours=d.getHours();
+let mins=d.getMinutes();
+let sec=d.getSeconds();
+let ms=d.getMilliseconds();
+let time=d.getTime();
+let day=d.getDay();
+document.write("Year: ",year,"<br>","Month: ",month+1,"<br>","Date: ",date,"<br>","Hours",hours,"<br>","Minutes: ",mins,"<br>","Seconds: ",sec,"<br>","Milliseconds: ",ms,"<br>","Time: ",time,"<br>","Day: ",day,"<br>");
